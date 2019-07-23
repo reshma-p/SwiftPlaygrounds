4fc3ff323a20ddc824e5411bafd1b19a0ababe9e
Reshma Pinto <reshma.pinto@igt.com>
2019-07-23 23:11:53 +0100

    added more logic to the fraction sums

--- NumberProblems.playground/Contents.swift
+++ NumberProblems.playground/Contents.swift
@@ -216,24 +216,64 @@ class ListSquareTest: XCTestCase{
 func sumFracts(_ l: [(Int, Int)]) -> (Int, Int)? {
     
     //-- For each element in the array, add them up
+    let sum = l.reduce((0,0)) { x,y in
+        convertToIrreducibleFraction(addTwofractions(x, y))
+//
+    }
     
-    return (0,0)
+//    reduce(0) { x, y in
+//        x + (y*y)
+//    }
+//    var fractionSum = addTwofractions()
+//    var irreducibleFraction = convertToIrreducibleFraction(fractionSum)
+    print("sum ofound \(sum)")
+    return sum
 }
 
 func addTwofractions(_ fraction1 : (Int,Int), _ fraction2 : (Int,Int)) -> (Int,Int){
-    
+    print("X => \(fraction1) Y => \(fraction2)")
     // Formula = a/b + c/d => (a*d + c*b)/ (b*d)
     let numerator = (fraction1.0 * fraction2.1) + (fraction1.1 * fraction2.0)
     let denominator = fraction1.1 * fraction2.1
+    
     return (numerator,denominator)
 }
 
+func convertToIrreducibleFraction(_ fraction1 : (Int,Int)) -> (Int,Int){
+    var numerator = fraction1.0
+    var denominator = fraction1.1
+    
+    var upperBound = (numerator < denominator ? numerator : denominator)/2
+    print("upperBound : \(upperBound)")
+    while upperBound-1 >= 1{
+        print("upperBound in loop : \(upperBound)")
+        if(numerator % upperBound == 0 && denominator % upperBound == 0 ){
+            //-- found the greatest common divisor
+            numerator /= upperBound
+            denominator /= upperBound
+            break;
+        }
+        upperBound -= 1
+    }
+    print("numerator : \(numerator) -- denominator : \(denominator)")
+    return (numerator,denominator)
+}
 
 class SumOfFractionsTest: XCTestCase{
     
     
+    func testing(_ l: [(Int, Int)], _ expected: (Int, Int)?) {
+        XCTAssert(sumFracts(l)! == expected!, "should return \(expected!)")
+    }
+    func testingNil(_ l: [(Int, Int)]) {
+        XCTAssertTrue(sumFracts(l) == nil, "Should return nil")
+    }
+    
     func testExample() {
-        
+        testing([(1, 2), (1, 3), (1, 4)], (13, 12))
+        testing([(1, 3), (5, 3)], (2, 1))
+        testingNil([])
+        testing([(1, 8), (1, 9)], (17, 72))
     }
     
 }
@@ -249,7 +289,7 @@ class AddTwoFractionsTest: XCTestCase{
     }
     
     func testNonFractions() {
-        XCTAssertTrue(assertEqualFractions(addTwofractions((1,2),(1,4)),(6,8)), "Not matching ")
+        XCTAssertTrue(assertEqualFractions(addTwofractions((1,2),(1,4)),(3,4)), "Not matching ")
     }
     
 }
@@ -264,4 +304,5 @@ CheckSquareRootTest.defaultTestSuite.run()
 ListSquareTest.defaultTestSuite.run()
 
 //-- Problem 3 tests
-AddTwoFractionsTest.defaultTestSuite.run()
+//AddTwoFractionsTest.defaultTestSuite.run()
+SumOfFractionsTest.defaultTestSuite.run()
